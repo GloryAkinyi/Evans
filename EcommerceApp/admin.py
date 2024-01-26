997bfec86afcda93cb2e7265b4122d6e840c063a
gloryakinyi <akinyiglory2@gmail.com>
2024-01-26 11:53:14 +0300

    initial commit

---
+++ EcommerceApp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from EcommerceApp.models import Member,ImageModel
+
+# Register your models here.
+admin.site.register(Member)
+admin.site.register(ImageModel)
---
+++ EcommerceApp/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+# Create your models here.
+class Member(models.Model):
+    fullname = models.CharField(max_length=100)
+    username = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.fullname
+
+
+class ImageModel(models.Model):
+    image = models.ImageField(upload_to='images/')
+    title = models.CharField(max_length=50)
+    price = models.CharField(max_length=50)
+
+
